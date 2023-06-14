a523cfce16de3cb0342b4d6532d7dc1df7625797
Marek Polak <markalexpolak@gmail.com>
2023-06-13 21:53:27 -0400

    bug fixing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 3008;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
